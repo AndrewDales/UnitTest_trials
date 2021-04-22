c8e7251febc8253a654364dd9a6f987213427fc2
Andrew Dales <andrew.dales@highgateschool.org.uk>
2021-04-22 14:54:58 +0100

    Added and tested extend_keyword method to repeat the keyword.

--- test_vigenere.py
+++ test_vigenere.py
@@ -17,23 +17,27 @@ class VigenereTestCase(unittest.TestCase):
         self.assertRaises(ValueError, VigenereCipher, "$%FG")
 
     def test_combine_characer(self):
-        self.assertEqual(self.cipher.combine_character("D", "E"), "H")
+        self.assertEqual(self.cipher._combine_character("D", "E"), "H")
 
     def test_combine_non_alpha(self):
-        self.assertRaises(ValueError, self.cipher.combine_character, "@", "t")
+        self.assertRaises(ValueError, self.cipher._combine_character, "@", "t")
         # The name of the function or method is required (without calling the function), followed by
         # *args and **kwargs
 
     def test_combine_character_mod(self):
-        self.assertEqual(self.cipher.combine_character("W", "F"), "B")
+        self.assertEqual(self.cipher._combine_character("W", "F"), "B")
 
     def test_combine_lower(self):
-        self.assertEqual(self.cipher.combine_character("y", "t"), "R")
+        self.assertEqual(self.cipher._combine_character("y", "t"), "R")
 
     def test_encode(self):
         encoded_text = self.cipher.encode("ENCODEINPYTHON")
         self.assertEqual(encoded_text, 'XECWQXUIVCRKHWA')
 
+    def test_extend_keyword(self):
+        self.assertEqual(self.cipher.extend_keyword(12), "TRAINTRAINTR")
+        cipher = VigenereCipher("CHEESE")
+        self.assertEqual(cipher.extend_keyword(18), "CHEESECHEESECHEESE")
 
 if __name__ == '__main__':
     unittest.main()
--- vigenere.py
+++ vigenere.py
@@ -9,10 +9,14 @@ class VigenereCipher:
         return 'XECWQXUIVCRKHWA'
 
     @staticmethod
-    def combine_character(plain_letter, keyword_letter):
+    def _combine_character(plain_letter, keyword_letter):
         if not (plain_letter.isalpha() and keyword_letter.isalpha()):
             raise ValueError("Plain letter and keyword letter must both be alphabetic (a-z or A-Z)")
         plain_num = ord(plain_letter.upper()) - ord('A')
         keyword_num = ord(keyword_letter.upper()) - ord('A')
         cipher_num = (plain_num + keyword_num) % 26
         return chr(cipher_num + ord('A'))
+
+    def extend_keyword(self, msg_length):
+        d, m = divmod(msg_length, len(self.keyword))
+        return self.keyword * d + self.keyword[:m]
